978f52ff45057fddde5fdc14e972ec356dd6dfa4
Billy Huang <10646003@ntub.edu.tw>
2022-09-30 17:15:47 +0800

    fix(scroll): scroll position calculate error

--- assets/scripts/scroll.js
+++ assets/scripts/scroll.js
@@ -6,17 +6,15 @@ const navSectionIds = [
   'courseSharing',
   'beautyTour',
   'citizenForum',
-];
+].reverse();
 const navSections = navSectionIds.map((item) => document.getElementById(item));
 
 window.onscroll = function () {
   const windowScrollHeight = document.documentElement.scrollTop;
+  const windowClientHeight = document.documentElement.clientHeight;
 
   for (const section of navSections) {
-    if (
-      windowScrollHeight >= section.offsetTop &&
-      windowScrollHeight <= section.offsetTop + section.offsetHeight
-    ) {
+    if (section.offsetTop - windowScrollHeight - windowClientHeight <= -100) {
       const navItems = document.querySelectorAll('.navbar > ul > li');
       navItems.forEach((item) => {
         const anchor = item.querySelector('a');
